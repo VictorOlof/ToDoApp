a4261a59e04d44d76a98389c03a267becc1604cf
Victor Olofsson <victorolofsson@hotmail.com>
2020-10-04 00:04:21 +0200

    Added ability to use a date for Task

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from datetime import date
+
+
+TODAY = date.today().strftime('%d-%m-%y')
+
+
 class TodoList:
     def __init__(self):
         self.todo_list = []  # store all task objects
@@ -9,19 +15,22 @@ class TodoList:
         # put task object in todo_list
         self.todo_list.append(task)
 
-    def show_todo_list(self):
+    def show_todo_list(self, date_input):
         # returns the todolist with task objects stored in todos
         for item in self.todo_list:
-            print(item.completed, "-", item.task_name)
+            if item.expire_date == date_input:
+                print(item.completed, "-", item.task_name)
+        # TODO: loop with enumerate for user to navigate?
 
 
 class Task:
-    def __init__(self, task_name):
-        self.task_name = task_name.capitalize()  # Name of task
-        self.completed = False  # If task is done
+    def __init__(self, task_name, expire_date):
+        self.task_name = task_name.capitalize()  # name of task
+        self.expire_date = expire_date  # date for task to show up
+        self.completed = False  # if task is done
 
     def __repr__(self):
-        return '{self.__class__.__name__}({self.task_name}, {self.completed}'.format(self=self)
+        return '{self.__class__.__name__}({self.task_name}, {self.expire_date}, {self.completed})'.format(self=self)
 
 
 def main():
@@ -31,7 +40,7 @@ def main():
     running = True
     while running:
         # show todolist
-        todolist.show_todo_list()
+        todolist.show_todo_list(TODAY)
 
         # show menu
         choice = int(input("1-Quit 2-Input task "))
@@ -40,7 +49,7 @@ def main():
             running = False
 
         elif choice == 2:  # input a new task
-            task = Task(input("Input task: "))
+            task = Task(input("Input task: "), TODAY)
             todolist.add_task(task)
 
 
