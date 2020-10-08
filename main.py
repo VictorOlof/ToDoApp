585ad02d37abd5bb8b577f0deae39b6d7140c993
Victor Olofsson <victorolofsson@hotmail.com>
2020-10-08 17:04:20 +0200

    Added ability to create a project

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ class ListInterface:
         return '{self.__class__.__name__}({self.project_lists})'.format(self=self)
 
     def add_project(self):
-        pass
+        name = input("Enter project name: ")
+        project = Project(name)
+        self.project_lists.append(project)
 
     def view_all_lists(self):
-        pass
+        for project in self.project_lists:
+            print(project.name)
+            for task in project.task_list:
+                print(task.name)
     
 
 class Project:
@@ -40,6 +45,18 @@ def main():
     # start of application
     li = ListInterface()
 
+    while True:
+        print("1. Add project")
+        print("2. Select project")
+        print("3. Quit application")
+        value = input("Select: ")
+
+        if value == "1":  # Add project
+            li.add_project()
+        if value == "2":  # Select project
+            li.view_all_lists()
+        if value == "3":  # Quit application
+            break
 
 if __name__ == '__main__':
     main()
