808181e0fb71f2d869c7f79d5f9b3c06561dc19e
Victor Olofsson <victorolofsson@hotmail.com>
2020-10-08 16:09:37 +0200

    Reorganized application in main.py

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
 class TodoList:
     def __init__(self):
-        self.todo_list = []  # store all task objects
+        self.task_list = []  # store all task objects
 
     def __repr__(self):
-        return '{self.__class__.__name__}({self.todo_list})'.format(self=self)
+        return '{self.__class__.__name__}({self.task_list})'.format(self=self)
 
     def add_task(self, task):
-        # put task object in todo_list
-        self.todo_list.append(task)
+        pass
 
-    def show_todo_list(self):
-        # returns the todolist with task objects stored in todos
-        for item in self.todo_list:
-            print(item.completed, "-", item.task_name)
+    def view_all_tasks(self):
+        pass
 
 
 class Task:
-    def __init__(self, task_name):
-        self.task_name = task_name.capitalize()  # Name of task
+    def __init__(self, name):
+        self.name = name.capitalize()  # Name of task
         self.completed = False  # If task is done
 
     def __repr__(self):
-        return '{self.__class__.__name__}({self.task_name}, {self.completed}'.format(self=self)
+        return '{self.__class__.__name__}({self.name}, {self.completed}'.format(self=self)
 
 
 def main():
     # start of application
-    todolist = TodoList()  # create a todolist using TodoList class
-
-    running = True
-    while running:
-        # show todolist
-        todolist.show_todo_list()
-
-        # show menu
-        choice = int(input("1-Quit 2-Input task "))
-
-        if choice == 1:  # quit application
-            running = False
-
-        elif choice == 2:  # input a new task
-            task = Task(input("Input task: "))
-            todolist.add_task(task)
+    todolist = TodoList
 
 
 if __name__ == '__main__':
