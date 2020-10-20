02fbbf65e5356a41210f8a1ee99a4587e8cf62eb
Victor Olofsson <victorolofsson@hotmail.com>
2020-10-20 15:11:03 +0200

    Property for name variable

--- task.py
+++ task.py
@@ -1,10 +1,21 @@
 class Task:
     def __init__(self, name):
-        self.name = name.capitalize()  # name of task
+        self.name = name  # name of task
         self.completed = False  # if task is done
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.name}, {self.completed}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 15:
+            raise ValueError("Name of task cannot be longer than 15 chars")
+        else:
+            self._name = name.capitalize()
 
     def set_completed(self):
         """Sets completed True->False or False->True"""
