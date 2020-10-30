c6d884f96880e32e889e25a43cd264990bc63218
Victor Olofsson <victorolofsson@hotmail.com>
2020-10-30 13:36:42 +0100

    Created project_interface.py

---
+++ project_interface.py
@@ -0,0 +1,18 @@
+class ProjectInterface:
+    """A Project class creates and handles all Task objects"""
+
+    def add_task(self):
+        """Creates a Task obj and saves in task_list"""
+        pass
+
+    def remove_task(self):
+        """Removes Task obj from task_list"""
+        pass
+
+    def mark_task(self):
+        """Change state of completed attribute in a task obj"""
+        pass
+
+    def view_all_tasks(self):
+        """Views all tasks in task_list with or without numbers"""
+        pass
